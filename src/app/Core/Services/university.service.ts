4880cba3dd107ed703d6b03aa542de8016b60f26
dorakadri <dora.kadri@esprit.tn>
2022-12-06 23:54:05 +0100

    2 commit

--- src/app/Core/Services/university.service.ts
+++ src/app/Core/Services/university.service.ts
@@ -1,51 +1,71 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
-import { Department } from '../Modals/department';
-import { University } from '../Modals/university';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { Department } from "../Modals/department";
+import { University } from "../Modals/university";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class UniversityService {
- public url=  environment.url
- public urldep=environment.urldep
-  constructor(private http: HttpClient) { }
+  public url = environment.url;
+  public urldep = environment.urldep;
+  constructor(private http: HttpClient) {}
 
-  getAllUniversity(){
-    return this.http.get<University[]>(this.url+"UniversityController/"+ "displayUniversities")
+  getAllUniversity() {
+    return this.http.get<University[]>(
+      this.url + "UniversityController/" + "displayUniversities"
+    );
   }
-  addUniversite(u:University){
-    return this.http.post(this.url+"UniversityController/"+"addUniversity",u)
+  addUniversite(u: University) {
+    return this.http.post(
+      this.url + "UniversityController/" + "addUniversity",
+      u
+    );
   }
-  delete(id: number){
-    return this.http.delete<any>(this.url+"UniversityController/"+"deleteuniversity/"+id)
+  delete(id: number) {
+    return this.http.delete<any>(
+      this.url + "UniversityController/" + "deleteuniversity/" + id
+    );
   }
   public updateUniversity(university: any) {
-    return this.http.put(this.url + "UniversityController/"+"updateUniversity", university);
+    return this.http.put(
+      this.url + "UniversityController/" + "updateUniversity",
+      university
+    );
   }
-  getUniversiteByID(idUniv:number){
-    return this.http.get<University>(this.url+ "UniversityController/"+"DisplayUniversitiesById/"+idUniv)
-  }
- 
-  assignDepartement(idUniv:number,idDepart:number[]){
-    
-    return this.http.put(this.url+"UniversityController/assignuniversityToDepartment/"+idUniv+"/"+idDepart,{})
-  }
-
-  getDepartement(){
-    return this.http.get<Department[]>(this.url+"DepartmentController/"+ "displayalldepartment") 
-  }
- 
-
- 
-  getDepartementbyId(idDepart:number){
-    return this.http.get<Department>(this.url+"DepartmentController/"+ "displdepartmentbyid/"+idDepart) 
+  getUniversiteByID(idUniv: number) {
+    return this.http.get<University>(
+      this.url + "UniversityController/" + "DisplayUniversitiesById/" + idUniv
+    );
   }
 
-  departementNonAffected(){
-    return this.http.get<Department[]>(this.url+"UniversityController/"+ "departementAffected")
+  assignDepartement(idUniv: number, idDepart: number[]) {
+    return this.http.put(
+      this.url +
+        "UniversityController/assignuniversityToDepartment/" +
+        idUniv +
+        "/" +
+        idDepart,
+      {}
+    );
   }
 
+  getDepartement() {
+    return this.http.get<Department[]>(
+      this.url + "DepartmentController/" + "displayalldepartment"
+    );
+  }
+
+  getDepartementbyId(idDepart: number) {
+    return this.http.get<Department>(
+      this.url + "DepartmentController/" + "displdepartmentbyid/" + idDepart
+    );
+  }
+
+  departementNonAffected() {
+    return this.http.get<Department[]>(
+      this.url + "UniversityController/" + "departementAffected"
+    );
+  }
 }
-
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,20 @@
-
-const contracts= "http://localhost:8081/SpringMVC/ContratApi/"
-const students= "http://localhost:8081/SpringMVC/controllerEtudiant/"
+const contracts = "http://localhost:8081/SpringMVC/ContratApi/";
+const students = "http://localhost:8081/SpringMVC/controllerEtudiant/";
 export const environment = {
-  production: false,
-  url:'http://localhost:8081/SpringMVC/',
-  urldep:'http://localhost:8081/SpringMVC/DepartmentController/',
-  endpoints:{
-    contracts:{
-      getAll:contracts+"getAll",
-      post:contracts+"post",
-      delete: contracts+"delete/",
-      get: contracts+"get/",
-      update: contracts+"update",
-      affectContractToStudent: contracts+"affectContratToEtudiant",
+  production: true,
+  url: "http://localhost:8081/SpringMVC/",
+  urldep: "http://localhost:8081/SpringMVC/DepartmentController/",
+  endpoints: {
+    contracts: {
+      getAll: contracts + "getAll",
+      post: contracts + "post",
+      delete: contracts + "delete/",
+      get: contracts + "get/",
+      update: contracts + "update",
+      affectContractToStudent: contracts + "affectContratToEtudiant",
     },
-    students:{
-      getAll:students+"DisplayStudents"
-    }
-  }
+    students: {
+      getAll: students + "DisplayStudents",
+    },
+  },
 };
